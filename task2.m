ca79d750ae8802b91a308f8860d91938fafd1e02
sgerlein24 <sofia_gerlein@brown.edu>
2024-03-13 12:53:15 -0400

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,7 +1,7 @@
 %importing file
-raw_data = readtable ('/Users/sofiagerlein/MATLAB/clps0950project/clps0950_rawdata.xlsx')
+
 %getting all of the participant ID's
-pivot_table = unique(raw_data{:,1});
+pivot_table = unique(proc_data{:,1});
 
 
 % how do i have it go through
@@ -15,19 +15,19 @@ pivot_table = unique(raw_data{:,1});
 % %at the end, take the table and export to excel
 
 %for each participant (this part needs to go into the loop i think)
-idx = strcmp(raw_data.resultId, '650352b0938c4e060cb94494') & strcmp(raw_data.Congruence, 'GC_Cong');%finds the index of every time it says that participant (cld be pp)
-newtable = raw_data(idx, :); %runs through raw data and only outputs when ^ is true
+idx = strcmp(proc_data.resultId, '650352b0938c4e060cb94494') & strcmp(proc_data.Congruence, 'GC_Cong');%finds the index of every time it says that participant (cld be pp)
+newtable = proc_data(idx, :); %runs through raw data and only outputs when ^ is true
 average = mean([newtable.responseTime]);
 %put this in the table
 
-for dd = 1:size(pivot_table,1) %go through the row
+for dd = 1:size(pivot_table,1) %go through the row for GC_Cong
    pid = pivot_table(dd,1);
-   idx = strcmp(raw_data.resultId, pid{1}); %& strcmp(raw_data.Congruence, 'GC_Cong');
-   conditiontable = raw_data(idx, :);
+   idx = strcmp(proc_data.resultId, pid{1}); %& strcmp(raw_data.Congruence, 'GC_Cong');
+   conditiontable = proc_data(idx, :);
    gc_cong_a = mean([conditiontable.responseTime]);
    pivot_table{dd, 2} = gc_cong_a;
-
 end
+
 
    %put gc_cong_a into pivot_table
 
