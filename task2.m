669ced653ed187c32b9f350dd320999dcdeadfed
sgerlein24 <sofia_gerlein@brown.edu>
2024-03-18 12:22:40 -0400

    Create task2.m

---
+++ task2.m
@@ -0,0 +1,52 @@
+
+%% 
+
+%getting all of the participant ID's
+pivot_table = unique(proc_data{:,1});
+
+%% 
+
+%name the variables at the top, do edits
+
+for dd = 1:size(pivot_table,1) %go through the row for GC_Cong
+  %GC_Cong
+   pid1 = pivot_table(dd,1);
+   idx1 = strcmp(proc_data.resultId, pid1{1}) & strcmp(proc_data.Congruence, 'GC_Cong');
+   conditiontable1 = proc_data(idx1, :);
+   gc_cong_a = mean([conditiontable1.LogRT]);
+   pivot_table{dd, 2} = gc_cong_a;
+end
+
+for dd = 1:size(pivot_table,1) %go through the row for GC_Incong
+  %GC_Incong
+   pid1 = pivot_table(dd,1);
+   idx1 = strcmp(proc_data.resultId, pid1{1}) & strcmp(proc_data.Congruence, 'GC_Incong');
+   conditiontable1 = proc_data(idx1, :);
+   gc_incong_a = mean([conditiontable1.LogRT]);
+   pivot_table{dd, 3} = gc_incong_a;
+end
+
+for dd = 1:size(pivot_table,1) %go through the row for CN_Cong
+  %CN_Cong
+   pid1 = pivot_table(dd,1);
+   idx1 = strcmp(proc_data.resultId, pid1{1}) & strcmp(proc_data.Congruence, 'CN_Cong');
+   conditiontable1 = proc_data(idx1, :);
+   CN_cong_a = mean([conditiontable1.LogRT]);
+   pivot_table{dd, 4} = CN_cong_a;
+end
+
+for dd = 1:size(pivot_table,1) %go through the row for CN_Incong
+  %CN_Incong
+   pid1 = pivot_table(dd,1);
+   idx1 = strcmp(proc_data.resultId, pid1{1}) & strcmp(proc_data.Congruence, 'CN_Incong');
+   conditiontable1 = proc_data(idx1, :);
+   cn_incong_a = mean([conditiontable1.LogRT]);
+   pivot_table{dd, 5} = cn_incong_a;
+end
+%% 
+
+varNames = {'resultid', 'GC_Cong', 'GC_Incong','CN_Cong','CN_Incong'};
+final_table = cell2table(pivot_table,'VariableNames',varNames);
+writetable(final_table, 'pivot_table.csv');
+
+%% 
