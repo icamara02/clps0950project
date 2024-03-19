bb529eefa1c3657bce28228af95458ba570be7ff
icamara02 <imani_camara@brown.edu>
2024-03-18 20:49:21 -0400

    Update task3.m
    Code to run a repeated subjects ANOVA and one-way anova (provides descriptive stats and creates box-plots for all conditions)

--- task3.m
+++ task3.m
@@ -1,7 +1,22 @@
+%% perform a Repeated-Meaures ANOVA
 
+data = readtable('pivot_table.csv'); % Load your data
 
-data = readtable ('pivot_table.csv');
-within_subjects = {'GC_Cong', 'GC_Incong', 'CN_Cong', 'CN_Incong'};
-rm = fitrm(data, 'GC_Cong, GC_Incong, CN_Cong, CN_Incong ~ 1', 'WithinDesign', within_subjects);
-ranovaResults = ranova(rm);
-disp(ranovaResults);
+within_subjects_factors = {'GC_Cong', 'GC_Incong', 'CN_Cong', 'CN_Incong'};% Define within-subjects factors (conditions)
+
+rm = fitrm(data, 'GC_Cong-CN_Incong~1', 'WithinDesign', within_subjects_factors);% Create a repeated measures model
+
+ranova_results = ranova(rm); % Perform repeated measures ANOVA
+disp (ranova_results);
+%% perform a One-way ANOVA
+data = readtable('pivot_table.csv'); %load data from the pivot table with average response times
+
+conditions = {'GC_Cong', 'GC_Incong', 'CN_Cong', 'CN_Incong'};%define the four conditions
+
+response_times = data{:, conditions}; % Extract response times for each condition
+
+[p, tbl, stats] = anova1(response_times);% Perform one-way ANOVA
+
+disp(tbl);% Display ANOVA table
+disp(stats);% Display ANOVA statistics
+disp(['p-value: ', num2str(p)]);% Display p-value
